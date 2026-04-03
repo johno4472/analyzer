cf0574a7a07c35b9342c68d15448730258226e8b
John <johnnephiolson@gmail.com>
2026-04-03 17:35:43 -0600

    Added an assertion and removed unnecessary imports

--- src/test/java/edu/byu/cs/sonar/MainTest.java
+++ src/test/java/edu/byu/cs/sonar/MainTest.java
@@ -2,10 +2,7 @@ package edu.byu.cs.sonar;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
-
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MainTest {
 
@@ -13,15 +10,12 @@ class MainTest {
     @Test
     void testMain() {
         String[] args = {"readMe1.txt", "readMe2.txt", "readMe3.txt", "5"};
-
         assertDoesNotThrow(() -> Main.main(args));
-
     }
 
     @Test
     void testError() {
       String[] args = {"readMe8.txt", "readMe2.txt", "readMe3.txt", "5"};
-
-      Main.main(args);
+      assertDoesNotThrow(() -> Main.main(args));
     }
 }
